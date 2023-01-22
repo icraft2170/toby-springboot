62a669b0c2b0ff2decac6649e228e6e8071b1b2c
Hero <icraft2170@gmail.com>
2023-01-22 17:17:41 +0900

    토비의 스프링 부트 실습: Embeded Tomcat 사용

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,16 @@
 package tobyspring.helloboot;
 
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
 public class HellobootApplication {
 	public static void main(String[] args) {
-		System.out.println("Hello Containerless Standalone Application");
+		TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+		// Tomcat, Jetty .. 등의 서블릿 컨테이너를 추상화한 PSA
+		WebServer webServer = serverFactory.getWebServer();
+		webServer.start();
 	}
 }
