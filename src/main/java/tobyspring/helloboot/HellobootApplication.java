c240454efe38daed37be9556a6a84ab76874390f
Hero <icraft2170@gmail.com>
2023-01-22 17:04:09 +0900

    토비의 스프링 부트 실습: Containerless init

---
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -0,0 +1,10 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class HellobootApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello Containerless Standalone Application");
+	}
+}
