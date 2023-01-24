afa16e69a71afcf2869968c0a5f83a1ebdf8db3e
Hero <icraft2170@gmail.com>
2023-01-24 12:53:51 +0900

    토비의 스프링 부트 실습: Annotation Mapping추가

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,15 +1,23 @@
 package tobyspring.helloboot;
 
 import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
+@RequestMapping
 public class HelloController {
+
   private final HelloService helloService;
 
   public HelloController(HelloService helloService) {
     this.helloService = helloService;
   }
 
+  @ResponseBody
+  @GetMapping("/hello")
   public String hello(String name) {
     return helloService.sayHello(Objects.requireNonNull(name));
   }
