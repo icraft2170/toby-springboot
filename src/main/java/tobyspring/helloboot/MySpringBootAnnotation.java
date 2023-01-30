7c84f462f4c4e68251956abe2b26deb1e9f3f054
Hero <icraft2170@gmail.com>
2023-01-30 16:12:52 +0900

    토비의 스프링 부트 실습: Config 를 ComponentScan 대상에서 제외하고 빈에 등록하기위해 @Import 사용

--- src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
+++ src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -6,11 +6,14 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import tobyspring.config.Config;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Configuration
 @ComponentScan
+@Import(Config.class)
 public @interface MySpringBootAnnotation {
 
 }
